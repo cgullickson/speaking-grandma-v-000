cd0bd183ed2ebf5f22014bc55945a8a244ee4474
Chris Gullickson <cgullickson5@gmail.com>
2017-11-20 22:35:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma (string)
   if string == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.upcase
